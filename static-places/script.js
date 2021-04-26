7416c6891cbbd7c86a838c4e2c2fdbf46c17fcf4
Nabib Ahmed <nahmed3536@gmail.com>
2021-04-26 15:36:37 -0400

    test4

--- static-places/script.js
+++ static-places/script.js
@@ -35,9 +35,9 @@ var models = [
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 90 0',
-        info: 'Dragonite, Lv. 100, HP 150/150',
+        scale: '0.02 0.02 0.02',
+        rotation: '0 0 0',
+        info: 'Dragonite, Lv. 101, HP 150/150',
     },
 ];
 
